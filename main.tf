d4e31823bf014ecc907c348484af51432fea9107
subhasis020299 <subhasisdas125@gmail.com>
2022-04-21 21:45:45 +0530

    added eks.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ module "networking" {
   source = "./modules/networking"
   aws_region = var.region
 }
+
+module "cluster" {
+  source = "./modules/cluster"
+  public_subnet_ids = module.networking.public_subnet_ids
+  private_subnet_ids = module.networking.private_subnet_ids
+}
--- modules/networking/vpc.tf
+++ modules/networking/vpc.tf
@@ -17,3 +17,11 @@ module "vpc" {
     Terraform = "true"
   } 
 }
+
+output "public_subnet_ids" {
+  value = "${module.vpc.public_subnet_ids}"
+}
+
+output "private_subnet_ids" {
+  value = "${module.vpc.private_subnet_ids}"
+}
