173a29264306cf352281a6ebcb70274512c39dc7
subhasis020299 <subhasisdas125@gmail.com>
2022-05-02 13:27:37 +0530

    added IAM OIDC provider

---
+++ modules/cluster/iam_oidc.tf
@@ -0,0 +1,12 @@
+#obtain TLS certificate for the OIDC provider
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+
+
+#create OpenID Connect Provider using TLS certificate
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
