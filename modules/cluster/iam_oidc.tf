e9d4321774662bfea7f18f82ec71438ccfab03c3
subhasis020299 <subhasisdas125@gmail.com>
2022-05-02 13:40:17 +0530

    added IAM role and policy for cluster autoscaler

--- modules/cluster/iam_oidc.tf
+++ modules/cluster/iam_oidc.tf
@@ -1,12 +1,12 @@
 #obtain TLS certificate for the OIDC provider
-data "tls_certificate" "eks" {
+data "tls_certificate" "tls" {
   url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
 
 
-#create OpenID Connect Provider using TLS certificate
-resource "aws_iam_openid_connect_provider" "eks" {
+#create OIDC Provider using TLS certificate
+resource "aws_iam_openid_connect_provider" "eks_ca_oidc_provider" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  thumbprint_list = [data.tls_certificate.tls.certificates[0].sha1_fingerprint]
   url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
